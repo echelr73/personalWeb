a0d976ac0d9642cbea1bfb5a999fe2a8a782151c
Martin Echeverria <martin.echeverria@accenture.com>
2023-02-07 16:52:44 -0300

    Approve in CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 import { TatetiComponent } from './tateti/tateti.component';
 
 const routes: Routes = [
-  { path: 'tateti', component: TatetiComponent}
+  { path: '', component: MenuComponent},
+  { path: 'menu', component: MenuComponent},
+  { path: 'menu/tateti', component: TatetiComponent}
 ];
 
 @NgModule({
